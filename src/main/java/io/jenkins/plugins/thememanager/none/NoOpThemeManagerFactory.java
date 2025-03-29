8109abe443bfcb87b55aa295ca16331a1eca12df
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2025-03-29 15:08:29 +0000

    Init

--- src/main/java/io/jenkins/plugins/thememanager/none/NoOpThemeManagerFactory.java
+++ src/main/java/io/jenkins/plugins/thememanager/none/NoOpThemeManagerFactory.java
@@ -27,7 +27,7 @@ public class NoOpThemeManagerFactory extends ThemeManagerFactory {
         @NonNull
         @Override
         public String getDisplayName() {
-            return "Default";
+            return "Light";
         }
 
         @Override
--- src/main/resources/io/jenkins/plugins/thememanager/header/main.js
+++ src/main/resources/io/jenkins/plugins/thememanager/header/main.js
@@ -1,25 +1,44 @@
 (function () {
-  const themeJson = document.getElementById('theme-manager-theme').text
-  const theme = JSON.parse(themeJson);
+  // const themeJson = document.getElementById('theme-manager-theme').text
+  // const theme = JSON.parse(themeJson);
 
-  if (theme.id && theme.id !== '') {
-    document.documentElement.dataset.theme = theme.id
+  // console.log(theme)
+
+  // if (theme.id && theme.id !== '') {
+  //   document.documentElement.dataset.theme = theme.id
+  // }
+
+
+  const themes = {
+    lightTheme: "light",
+    darkTheme: "dark",
   }
-  window.isSystemRespectingTheme = theme.respect_system_appearance
 
-  const propertiesJson = document.getElementById('theme-manager-properties')
-  // may not be present, e.g. on BlueOcean where the theme is not marked as BlueOcean compatible
-  if (propertiesJson) {
-    const parsedProperties = JSON.parse(propertiesJson.text);
-
-    window.getThemeManagerProperty = function (plugin, propertyName) {
-      const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches
-
-      let propertyNameNormalised = propertyName
-      if (isSystemRespectingTheme) {
-        propertyNameNormalised = isDark ? `${propertyName}-dark` : `${propertyName}-light`
-      }
-      return parsedProperties[`${plugin}:${propertyNameNormalised}`]
+  const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+  darkThemeMq.addEventListener("change",e => {
+    if (e.matches) {
+      document.documentElement.dataset.theme = themes.darkTheme;
+    } else {
+      document.documentElement.dataset.theme = themes.lightTheme;
     }
-  }
+  });
+  darkThemeMq.dispatchEvent(new Event("change"));
+
+  // window.isSystemRespectingTheme = theme.respect_system_appearance
+  //
+  // const propertiesJson = document.getElementById('theme-manager-properties')
+  // may not be present, e.g. on BlueOcean where the theme is not marked as BlueOcean compatible
+  // if (propertiesJson) {
+  //   const parsedProperties = JSON.parse(propertiesJson.text);
+  //
+  //   window.getThemeManagerProperty = function (plugin, propertyName) {
+  //     const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+  //
+  //     let propertyNameNormalised = propertyName
+  //     if (isSystemRespectingTheme) {
+  //       propertyNameNormalised = isDark ? `${propertyName}-dark` : `${propertyName}-light`
+  //     }
+  //     return parsedProperties[`${plugin}:${propertyNameNormalised}`]
+  //   }
+  // }
 })()
